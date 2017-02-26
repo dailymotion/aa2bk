b6d990a4d05736c7d65e18e000d4ef3846706218
Martin Bonnin <martin@mbonnin.net>
2017-02-26 15:08:42 +0100

    initial commit

---
+++ src/com/dailymotion/aa2bk/AA2BK.java
@@ -0,0 +1,174 @@
+package com.dailymotion.aa2bk;
+
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.xml.XmlTokenImpl;
+import com.intellij.psi.xml.XmlToken;
+import com.intellij.psi.xml.XmlTokenType;
+
+import java.lang.reflect.Executable;
+import java.util.*;
+
+public class AA2BK extends AnAction {
+
+    private Project mProject;
+    private Set<String> mProcessedNames = new HashSet<>();
+    private Set<String> mProcessedQualifedNames = new HashSet<>();
+
+    private void processClass(PsiClass psiClass) {
+        System.out.println("processClass " + psiClass);
+
+        PsiAnnotation psiAnnotation = Util.getAnnotation(psiClass, "EViewGroup");
+
+        PsiElement psiNameValuePair = Util.findElement(psiAnnotation, PsiNameValuePair.class);
+        if (psiNameValuePair == null) {
+            System.err.println("cannot find layoutId for " + psiClass);
+            return;
+        }
+
+        String layoutId = psiNameValuePair.getText();
+        psiAnnotation.delete();
+
+        String body = String.format("" +
+                        "public static %s build(Context context) {" +
+                        "   return (%s) LayoutInflater.from(context).inflate(%s, null);" +
+                        "}",
+                psiClass.getName(),
+                psiClass.getName(),
+                layoutId);
+
+        PsiMethod method = JavaPsiFacade.getElementFactory(mProject).createMethodFromText(body, psiClass);
+        psiClass.add(method);
+
+        Util.addImportIfNeeded(mProject, psiClass.getContainingFile(), "android.view.LayoutInflater");
+
+        for (PsiElement child : psiClass.getChildren()) {
+            if (child instanceof PsiField) {
+                processField((PsiField) child);
+            }
+        }
+
+        PsiMethod onFinishInflatePsiMethod = null;
+        PsiMethod afterViewsPsiMethod = null;
+        for (PsiElement child: psiClass.getChildren()) {
+            if (child instanceof PsiMethod) {
+                if (((PsiMethod) child).getName().equals("onFinishInflate")) {
+                    onFinishInflatePsiMethod = (PsiMethod) child;
+                    break;
+                } else {
+                    PsiAnnotation psiAnnotation2 = Util.getAnnotation(child, "AfterViews");
+                    if (psiAnnotation2 != null) {
+                        afterViewsPsiMethod = (PsiMethod) child;
+                    }
+                }
+            }
+        }
+
+        if (onFinishInflatePsiMethod == null) {
+            body = "@Override" +
+                    " protected void onFinishInflate() {" +
+                    "super.onFinishInflate();" +
+                    "}";
+
+            onFinishInflatePsiMethod = JavaPsiFacade.getElementFactory(mProject).createMethodFromText(body, psiClass);
+            onFinishInflatePsiMethod = (PsiMethod) psiClass.add(onFinishInflatePsiMethod);
+        }
+
+        PsiCodeBlock codeBlock = Util.findElement(onFinishInflatePsiMethod, PsiCodeBlock.class);
+        codeBlock.addAfter(JavaPsiFacade.getElementFactory(mProject).createStatementFromText("ButterKnife.bind(this);", psiClass), codeBlock.getLastBodyElement());
+        try {
+            Util.addImportIfNeeded(mProject, psiClass.getContainingFile(), "butterknife.ButterKnife");
+            Util.addImportIfNeeded(mProject, psiClass.getContainingFile(), "butterknife.BindView");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (afterViewsPsiMethod != null) {
+            PsiCodeBlock codeBlock1 = Util.findElement(afterViewsPsiMethod, PsiCodeBlock.class);
+            for (int i = 1; i < codeBlock1.getChildren().length - 1; i++) {
+                codeBlock.addAfter(codeBlock1.getChildren()[i], codeBlock.getLastBodyElement());
+            }
+
+            afterViewsPsiMethod.delete();
+        }
+
+        mProcessedNames.add(psiClass.getName());
+        mProcessedQualifedNames.add(psiClass.getQualifiedName());
+    }
+
+    private void processField(PsiField psiField) {
+        List<PsiAnnotation> annotationList = Util.findElements(psiField, PsiAnnotation.class);
+        PsiAnnotation annotation = null;
+
+        for (PsiElement element : annotationList) {
+            if (element.getText().equals("@ViewById")) {
+                annotation = (PsiAnnotation) element;
+                break;
+            }
+        }
+
+        if (annotation == null) {
+            return;
+        }
+
+        for (PsiElement child : psiField.getChildren()) {
+            if (child instanceof PsiIdentifier) {
+                String fieldName = child.getText();
+                String a = String.format("@BindView(R.id.%s)", fieldName);
+                PsiAnnotation newAnnotation = JavaPsiFacade.getInstance(mProject).getElementFactory().createAnnotationFromText(a, psiField);
+                annotation.replace(newAnnotation);
+                return;
+            }
+        }
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+        mProject = event.getData(PlatformDataKeys.PROJECT);
+        WriteCommandAction.runWriteCommandAction(mProject, () -> {
+            Util.traverseFiles(mProject, new HashSet<>(Arrays.asList(".java")), psiFile -> {
+                for (PsiElement psiElement: psiFile.getChildren()) {
+                    if (psiElement instanceof PsiClass) {
+                        if (Util.getAnnotation(psiElement, "EViewGroup") != null) {
+                            processClass((PsiClass) psiElement);
+                        }
+                    }
+                }
+            });
+            Util.traverseFiles(mProject, new HashSet<>(Arrays.asList(".java", ".xml")), psiFile -> {
+                processIdentifiers(psiFile);
+            });
+        });
+    }
+
+    private void processIdentifiers(PsiFile psiFile) {
+        psiFile.accept(new PsiRecursiveElementVisitor() {
+            @Override
+            public void visitElement(PsiElement element) {
+                super.visitElement(element);
+
+                if (element instanceof PsiIdentifier) {
+                    String identifier = element.getText();
+                    if (identifier.charAt(identifier.length() - 1) == '_') {
+                        String newIdentifier = identifier.substring(0, identifier.length() - 1);
+                        if (mProcessedNames.contains(newIdentifier)) {
+                            System.out.println("rename " + identifier + " -> " + newIdentifier);
+                            element.replace(JavaPsiFacade.getInstance(mProject).getElementFactory().createIdentifier(newIdentifier));
+                        }
+                    }
+                } else if (element instanceof XmlToken && ((XmlToken) element).getTokenType().equals(XmlTokenType.XML_NAME)) {
+                    String identifier = element.getText();
+                    if (identifier.charAt(identifier.length() - 1) == '_') {
+                        String newIdentifier = identifier.substring(0, identifier.length() - 1);
+                        if (mProcessedQualifedNames.contains(newIdentifier)) {
+                            final ElementManipulator<PsiElement> manipulator = ElementManipulators.getManipulator(element);
+                            manipulator.handleContentChange(element, new TextRange(0, identifier.length()), newIdentifier);
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ src/com/dailymotion/aa2bk/Util.java
@@ -0,0 +1,110 @@
+package com.dailymotion.aa2bk;
+
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ContentIterator;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+
+import javax.tools.JavaFileObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by martin on 2/24/17.
+ */
+public class Util {
+    public static void addImportIfNeeded(Project project, PsiFile psiFile, String qualifiedName) {
+        PsiClass layoutInflaterPsiClass = JavaPsiFacade.getInstance(project).findClass(qualifiedName, GlobalSearchScope.allScope(project));
+        PsiImportList psiImportList = findElement(psiFile, PsiImportList.class);
+        for (PsiElement child: psiImportList.getChildren()) {
+            PsiJavaCodeReferenceElement e = findElement(child, PsiJavaCodeReferenceElement.class);
+            if (e != null && e.getText().equals(qualifiedName)) {
+                // we already have the reference, to not add it
+                return;
+            }
+        }
+
+        psiImportList.add(JavaPsiFacade.getElementFactory(project).createImportStatement(layoutInflaterPsiClass));
+    }
+
+    public static String getAnnotationName(PsiAnnotation psiAnnotation) {
+        for (PsiElement child : psiAnnotation.getChildren()) {
+            if (child instanceof PsiJavaCodeReferenceElement) {
+                for (PsiElement child2 : child.getChildren()) {
+                    if (child2 instanceof PsiIdentifier) {
+                        return child2.getText();
+                    }
+                }
+            }
+        }
+        return "?";
+    }
+    public static PsiAnnotation getAnnotation(PsiElement element, String name) {
+        for (PsiElement child : element.getChildren()) {
+            if (child instanceof PsiModifierList) {
+                for (PsiElement child2 : child.getChildren()) {
+                    if (child2 instanceof PsiAnnotation) {
+                        if (name.equals(getAnnotationName((PsiAnnotation) child2))) {
+                            return (PsiAnnotation) child2;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public  static <T extends PsiElement> T findElement(PsiElement psiElement, Class<T> clazz) {
+
+        List<T> list = findElements(psiElement, clazz);
+        if (list.size() > 0) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public  static <T extends PsiElement> List<T> findElements(PsiElement psiElement, Class<T> clazz) {
+        final List<T> list = new ArrayList<T>();
+        psiElement.accept(new PsiRecursiveElementVisitor() {
+            @Override
+            public void visitElement(PsiElement element) {
+                super.visitElement(element);
+                if (clazz.isInstance(element)) {
+                    list.add((T) element);
+                }
+            }
+        });
+
+        return list;
+    }
+
+
+    interface JavaFileIterator {
+        void processFile(PsiFile psiFile);
+    }
+
+    public static void traverseFiles(Project project, Set<String> extensions, JavaFileIterator iterator) {
+        ProjectFileIndex.SERVICE.getInstance(project).iterateContent(fileOrDir -> {
+            String n = fileOrDir.toString();
+            int i = n.lastIndexOf('.');
+            if (i < 0) {
+                return true;
+            }
+            String ext = n.substring(i);
+            if (extensions.contains(ext)) {
+                PsiFile psiFile = PsiManager.getInstance(project).findFile(fileOrDir);
+                if (psiFile != null) {
+                    iterator.processFile(psiFile);
+                } else {
+                    System.err.println("could not find psiFile for: " + n);
+                }
+            }
+            return true;
+        });
+    }
+}
